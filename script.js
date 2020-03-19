f5e71a2c450f3eeb83c0c804adee47411d3374c7
yun09 <57771525+yun09@users.noreply.github.com>
2020-03-19 13:57:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+  <scipt>
+      document.getElementById("first").src = "flower2.png";
+
+      
+
+    
+    <div><p>Your are great!</p></div>
+
+    <div>
+      <span id="new_element">
+        Here should appear our flower:
+      </span>
+      </div>
+
+  </script>
